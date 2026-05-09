a65bd0d6637ce12725e19f3af0028103da40368d
Jack Conley <128567631+jacco44@users.noreply.github.com>
2026-05-09 10:36:31 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,15 @@
+"""Serve the USD-Rowing login page."""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
